6dd203a0dd867a1fd8a90e50dc9601a0636684df
Honza Horak <hhorak@janhorak-notebook.(none)>
2011-12-27 22:43:11 +0100

    allow displaying codebook in left menu in admin

--- app/packages/Abstract/controllers/AbstractCodebookController.php
+++ app/packages/Abstract/controllers/AbstractCodebookController.php
@@ -3,6 +3,10 @@
 
 class AbstractCodebookController extends AbstractDefaultController {
 	
+	public function getLinksAdminMenuLeft() {
+		return AbstractAdminController::getLinksAdminMenuLeft($this);
+	}
+
 }
 
 ?>
