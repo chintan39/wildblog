52732410f1e8ba01bcc880b4feeebb9f09eda15f
Honza Horak <horak.honza@gmail.com>
2012-11-10 11:54:07 +0100

    version bump because of attendance package added
    Conflicts:
    	app/config/default.inc

---
+++ app/packages/Attendance/models/AttendanceParticipantsModel.php
@@ -0,0 +1,47 @@
+<?php
+/*
+    Copyright (C) 2012  Honza Horak <horak.honza@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+class AttendanceParticipantsModel extends AbstractPagesModel {
+
+	var $package = 'Attendance';
+	var $icon = 'comment', $table = 'participants';
+
+    protected function attributesDefinition() {
+    	
+    	parent::attributesDefinition();
+    	
+    	$this->addMetaData(AtributesFactory::stdAccountEmail()
+    		->addRestrictions(Restriction::R_NOT_EMPTY | Restriction::R_UNIQUE));
+    	
+    	$this->addMetaData(AtributesFactory::stdFirstname());
+    	
+    	$this->addMetaData(AtributesFactory::stdSurname());
+    	
+    }
+
+    protected function relationsDefinition() {
+    	
+    	parent::relationsDefinition();
+    	
+        $this->addCustomRelationMany('AttendanceEventsModel', 'AttendanceEventsParticipantsModel', 'participant', 'event');
+    }
+
+} 
+
+?>
